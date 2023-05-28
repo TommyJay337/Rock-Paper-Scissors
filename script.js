fddfae06b1e93fcbf84f37fd9487b4781f104957
Thomas <thomas.c.johnston1998@gmail.com>
2023-05-28 00:26:03 -0600

    init commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+
+    let options = ["rock","paper","scissors"];
+    let randomOptions = options[Math.floor(Math.random()*options.length)]
+
+    return randomOptions
+
+}
+
+console.log(getComputerChoice());
+
+function playRound(playerSelection,computerSelection) {
+    IF(){
+        
+    }
+
+    return "You Lose! Paper beats Rock"
+}
